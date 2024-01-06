77c690a840113e4cfa5bdb5cc1c7b452b0b63375
Josh Gauthreaux <joshuagauthreaux@gmail.com>
2024-01-05 19:00:08 -0900

    update db.js to use Client instead of Pool

--- db.js
+++ db.js
@@ -1,11 +1,22 @@
-const { Pool } = require('pg');
+const { Client } = require('pg');
 
-const pool = new Pool({
-  user: process.env.PSQLUSER,
-  host: 'localhost',
-  database: 'gotro_db',
-  password: process.env.PSQLPASS,
-  port: 5432,
-});
+const clientConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false, // Required for Heroku
+      },
+    }
+  : {
+      user: process.env.PSQLUSER,
+      host: 'localhost',
+      database: 'gotro_db',
+      password: process.env.PSQLPASS,
+      port: 5432,
+    };
 
-module.exports = pool;
+const client = new Client(clientConfig);
+
+client.connect();
+
+module.exports = client;
