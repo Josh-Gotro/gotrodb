4f91d6cbb971cb9f7b62443780e91008d43f7bf2
josh-gotro <josh.gauthreaux@wostmann.com>
2024-01-08 09:30:55 -0900

    create server endpoints for retrieving current kiln firing, historical firings, and a post request to update or create a firing.

--- server.js
+++ server.js
@@ -88,6 +88,86 @@ app.get('/plaster-calculations', async (req, res) => {
   }
 });
 
+//  GET endpoint to retrieve the current ceramic kiln firing
+app.get('/current-ceramic-firing', (req, res) => {
+  pool.query(
+    'SELECT * FROM public.kiln_ceramic_records WHERE firing_complete = false LIMIT 1',
+    (error, results) => {
+      if (error) {
+        res.status(500).json({ error: error.toString() });
+      } else {
+        res.json(results.rows[0]);
+      }
+    }
+  );
+});
+
+//  GET endpoint to retrieve the completed ceramic kiln firings
+app.get('/ceramic-firings', (req, res) => {
+  pool.query(
+    'SELECT * FROM public.kiln_ceramic_records WHERE firing_complete = true',
+    (error, results) => {
+      if (error) {
+        res.status(500).json({ error: error.toString() });
+      } else {
+        res.json(results.rows);
+      }
+    }
+  );
+});
+
+// POST endpoint to update the current ceramic kiln firing if it exists, otherwise create a new one
+app.post('/ceramic-firings', (req, res) => {
+  const {
+    id,
+    room_temp,
+    low_fire_start_time,
+    medium_fire_start_time,
+    high_fire_start_time,
+    kiln_turn_off_time,
+    loading_notes,
+    firing_complete,
+    rating,
+    cone_type,
+  } = req.body;
+
+  const query = `
+        INSERT INTO public.kiln_ceramic_records(id, room_temp, low_fire_start_time, medium_fire_start_time, high_fire_start_time, kiln_turn_off_time, loading_notes, firing_complete, rating, cone_type)
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+        ON CONFLICT (id) DO UPDATE SET
+        room_temp = excluded.room_temp,
+        low_fire_start_time = excluded.low_fire_start_time,
+        medium_fire_start_time = excluded.medium_fire_start_time,
+        high_fire_start_time = excluded.high_fire_start_time,
+        kiln_turn_off_time = excluded.kiln_turn_off_time,
+        loading_notes = excluded.loading_notes,
+        firing_complete = excluded.firing_complete,
+        rating = excluded.rating,
+        cone_type = excluded.cone_type
+    `;
+
+  const values = [
+    id,
+    room_temp,
+    low_fire_start_time,
+    medium_fire_start_time,
+    high_fire_start_time,
+    kiln_turn_off_time,
+    loading_notes,
+    firing_complete,
+    rating,
+    cone_type,
+  ];
+
+  pool.query(query, values, (error, results) => {
+    if (error) {
+      res.status(500).json({ error: error.toString() });
+    } else {
+      res.json({ status: 'success', message: 'Record updated successfully' });
+    }
+  });
+});
+
 // Starting the server
 const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
